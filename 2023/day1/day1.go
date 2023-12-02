7d043987364c7ad0b56dafb36ace1dfc713e4e60
Clay26 <5711623+Clay26@users.noreply.github.com>
2023-12-02 20:58:13 +0000

    Read input

--- 2023/day1/day1.go
+++ 2023/day1/day1.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	file, err := os.Open("input_test_1.txt")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var input []string
+	for scanner.Scan() {
+		// Print each line
+		input = append(input, scanner.Text())
+	}
+
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading from file:", err)
+	}
 }
