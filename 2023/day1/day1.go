8a29c8a553a92825f3a68868c4ccecf348b8612b
Clay26 <5711623+Clay26@users.noreply.github.com>
2023-12-03 20:05:01 +0000

    Convert funcs to private

--- 2023/day1/day1.go
+++ 2023/day1/day1.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"AoC2023/fileutils"
-	"fmt"
+	"AoC2023/utils"
 	"regexp"
 	"strconv"
 )
 
 func main() {
 	pt1Ans := Part1("input_1.txt")
-	fmt.Println(pt1Ans)
+	pt2Ans := Part2("input_test_2.txt")
+	utils.PrintAnswer(1, 1, pt1Ans)
 }
 
 func Part1(inputFileName string) int {
-	input, err := fileutils.ReadFile(inputFileName)
+	input, err := utils.ReadFile(inputFileName)
 	if err != nil {
 		return -1
 	}
 
 	sum := 0
 	for _, inputLine := range input {
-		sum += ConvertFoundStrToNum(FindAllNums(inputLine))
+		sum += convertFoundStrToNum(findAllNums(inputLine))
 		if err != nil {
 			return -1
 		}
@@ -29,12 +29,16 @@ func Part1(inputFileName string) int {
 	return sum
 }
 
-func FindAllNums(inputLine string) []string {
+func Part2(inputFileName string) int {
+	return -1
+}
+
+func findAllNums(inputLine string) []string {
 	numRe := regexp.MustCompile(`(\d)`)
 	return numRe.FindAllString(inputLine, -1)
 }
 
-func ConvertFoundStrToNum(inputStr []string) int {
+func convertFoundStrToNum(inputStr []string) int {
 	firstDigitStr := inputStr[0]
 	secondDigitStr := inputStr[len(inputStr)-1]
 	foundNum, err := strconv.Atoi(firstDigitStr + secondDigitStr)
---
+++ 2023/utils/printutils.go
@@ -0,0 +1,7 @@
+package utils
+
+import "fmt"
+
+func PrintAnswer(day int, part int, ans int) {
+	fmt.Printf("Day %d, Part %d Answer: %d\n", day, part, ans)
+}
