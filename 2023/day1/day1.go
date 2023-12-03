1c8619db4386cb2a6884b470e26d21a7c705323b
Clay26 <5711623+Clay26@users.noreply.github.com>
2023-12-03 20:23:07 +0000

    Add initial part 2 solution

--- 2023/day1/day1.go
+++ 2023/day1/day1.go
@@ -8,8 +8,9 @@ import (
 
 func main() {
 	pt1Ans := Part1("input_1.txt")
-	pt2Ans := Part2("input_test_2.txt")
+	pt2Ans := Part2("input_1.txt")
 	utils.PrintAnswer(1, 1, pt1Ans)
+	utils.PrintAnswer(1, 2, pt2Ans)
 }
 
 func Part1(inputFileName string) int {
@@ -30,7 +31,20 @@ func Part1(inputFileName string) int {
 }
 
 func Part2(inputFileName string) int {
-	return -1
+	input, err := utils.ReadFile(inputFileName)
+	if err != nil {
+		return -1
+	}
+
+	sum := 0
+	for _, inputLine := range input {
+		sum += convertFoundStrToNum(findAllNumsAndWords(inputLine))
+		if err != nil {
+			return -1
+		}
+	}
+
+	return sum
 }
 
 func findAllNums(inputLine string) []string {
@@ -38,12 +52,44 @@ func findAllNums(inputLine string) []string {
 	return numRe.FindAllString(inputLine, -1)
 }
 
+func findAllNumsAndWords(inputLine string) []string {
+	numRe := regexp.MustCompile(`(\d|one|two|three|four|five|six|seven|eight|nine)`)
+	return numRe.FindAllString(inputLine, -1)
+}
+
 func convertFoundStrToNum(inputStr []string) int {
-	firstDigitStr := inputStr[0]
-	secondDigitStr := inputStr[len(inputStr)-1]
+	firstDigitStr := convertFoundStrToDigit(inputStr[0])
+	secondDigitStr := convertFoundStrToDigit(inputStr[len(inputStr)-1])
 	foundNum, err := strconv.Atoi(firstDigitStr + secondDigitStr)
 	if err != nil {
 		return -1
 	}
 	return foundNum
+}
+
+func convertFoundStrToDigit(inputStr string) string {
+	switch inputStr {
+	case "zero":
+		return "0"
+	case "one":
+		return "1"
+	case "two":
+		return "2"
+	case "three":
+		return "3"
+	case "four":
+		return "4"
+	case "five":
+		return "5"
+	case "six":
+		return "6"
+	case "seven":
+		return "7"
+	case "eight":
+		return "8"
+	case "nine":
+		return "9"
+	default:
+		return inputStr
+	}
 }
